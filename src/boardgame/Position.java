92452840a51ce08466532b2e7ace944607f2f05a
gcsvieira <74613475+gcsvieira@users.noreply.github.com>
2022-11-12 22:07:18 -0300

    first class position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Position {
+    private Integer row;
+    private Integer column;
+
+    public Position(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public Integer getRow() {
+        return this.row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return this.column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+
+}
